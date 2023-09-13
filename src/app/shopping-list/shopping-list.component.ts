6a539b8e8b81d8807867c569d09ec66ca2f4c32f
Saksham Gupta <saksham757gupta@gmail.com>
2023-09-13 22:41:59 +0530

    Section 5

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.models';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomatoes', 5),
+    new Ingredient('Onions', 3),
+    new Ingredient('Bell Peppers', 2),
+    new Ingredient('Garlic Cloves', 10),
+    new Ingredient('Cilantro', 1),
+  ];
 
 }
